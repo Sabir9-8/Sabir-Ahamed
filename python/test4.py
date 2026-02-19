164b05e9c02e1ff6e781b8278b3f5f4ab56d358b
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-02-20 01:45:40 +0530

    Correct syntax errors in test4.py
    Fix syntax errors in divide function and control structures.

---
+++ python/test4.py
@@ -0,0 +1,16 @@
+def divide(a, b):
+    if b = 0:
+        return "Cannot divide"
+    return a / b
+
+numbers = [1,2,3]
+print(numbers[5])
+
+for i in range(5)
+    print(i)
+
+x == 10
+if x > 5:
+print("Greater")
+
+open("file.txt")
